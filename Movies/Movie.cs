67e9f9e2c47855e88179082b3b20c4e9c850a520
Elyse Roth <elysemorganroth@gmail.com>
2021-09-27 22:32:33 -0400

    Add project files.

---
+++ Movies/Movie.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies
+{
+    class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Rating { get; set; } = "PG"; //how you can enter a default value
+        public int Released { get; set; } //year only
+        public int Runtime { get; set; } //in mintues
+        public string Director { get; set; }
+
+        public Movie() { } //default constructor, add now so don't get error later
+        public Movie(int id, string title, string genre, string rating,
+            int released, int runtime, string director)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Genre = genre;
+            this.Rating = rating;
+            this.Released = released;
+            this.Runtime = runtime;
+            this.Director = director; }
+
+            public int RuntimeMins(int hours, int mins) 
+        { 
+            return (hours * 60) + mins;
+        }
+
+
+            public void Print(int id, string title, string genre, string rating,
+            int released, int runtime, string director)
+            {
+                Console.WriteLine($"Here's everything you need to know about {title}: Genre: {genre}, Rating: {rating}, " +
+                    $"Year Released: {released}, Runtime in Minutes: {runtime}, Director: {director}.");
+            }
+
+            //think about own life and build class for it and add class for it
+        }
+      
+}
+
---
+++ Movies/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var LegallyBlonde = new Movie();
+            LegallyBlonde.Id = 1;
+            LegallyBlonde.Title = "Legally Blonde";
+            LegallyBlonde.Genre = "Comedy";
+            LegallyBlonde.Rating = "PG-13";
+            LegallyBlonde.Released = 2001;
+            LegallyBlonde.Runtime = LegallyBlonde.RuntimeMins(1, 36);
+            LegallyBlonde.Director = "Robert Luketic";
+            LegallyBlonde.Print(LegallyBlonde.Id, LegallyBlonde.Title, LegallyBlonde.Genre, LegallyBlonde.Rating,
+            LegallyBlonde.Released, LegallyBlonde.Runtime, LegallyBlonde.Director);
+            var Clueless = new Movie();
+            Clueless.Id = 2;
+            Clueless.Title = "Clueless";
+            Clueless.Genre = "Comedy";
+            Clueless.Rating = "PG-13";
+            Clueless.Released = 1995;
+            Clueless.Runtime = Clueless.RuntimeMins(1, 37);
+            Clueless.Director = "Amy Heckerling";
+            Clueless.Print(Clueless.Id, Clueless.Title, Clueless.Genre, Clueless.Rating, Clueless.Released, Clueless.Runtime, Clueless.Director);
+            var CenterStage = new Movie();
+            CenterStage.Id = 3;
+            CenterStage.Title = "Center Stage";
+            CenterStage.Genre = "Drama";
+            CenterStage.Rating = "PG-13";
+            CenterStage.Released = 2000;
+            CenterStage.Runtime = CenterStage.RuntimeMins(1, 55);
+            CenterStage.Director = "Nicholas Hytner";
+            CenterStage.Print(CenterStage.Id, CenterStage.Title, CenterStage.Genre, CenterStage.Rating,
+                CenterStage.Released, CenterStage.Runtime, CenterStage.Director);
+            var TenThings = new Movie();
+            TenThings.Id = 4;
+            TenThings.Title = "10 Things I Hate About You";
+            TenThings.Genre = "Comedy";
+            TenThings.Rating = "PG-13";
+            TenThings.Released = 1999;
+            TenThings.Runtime = TenThings.RuntimeMins(1, 37);
+            TenThings.Director = "Gil Junger";
+            TenThings.Print(TenThings.Id, TenThings.Title, TenThings.Genre, TenThings.Rating,
+                TenThings.Released, TenThings.Runtime, TenThings.Director);
+            var HowToLoseAGuy = new Movie();
+            HowToLoseAGuy.Id = 5;
+            HowToLoseAGuy.Title = "How To Los a Guy in 10 Days";
+            HowToLoseAGuy.Genre = "Romantic Comedy";
+            HowToLoseAGuy.Rating = "PG-13";
+            HowToLoseAGuy.Released = 2003;
+            HowToLoseAGuy.Runtime = HowToLoseAGuy.RuntimeMins(1, 56);
+            HowToLoseAGuy.Director = "Donald Petrie";
+            HowToLoseAGuy.Print(HowToLoseAGuy.Id, HowToLoseAGuy.Title, HowToLoseAGuy.Genre, HowToLoseAGuy.Rating,
+                HowToLoseAGuy.Released, HowToLoseAGuy.Runtime, HowToLoseAGuy.Director);
+
+
+
+
+
+
+        }
+    }
+}
