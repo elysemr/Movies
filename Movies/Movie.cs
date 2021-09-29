bdd5c38bcbdbf79ca3c4308c1b7e31f9db846d11
Elyse Roth <elysemorganroth@gmail.com>
2021-09-29 12:11:48 -0400

    made changes when went over in class

--- Movies/Movie.cs
+++ Movies/Movie.cs
@@ -8,6 +8,9 @@ namespace Movies
 {
     class Movie
     {
+        private static int NextId { get; set; } = 1; //this is defined as a property not with the class so it's set to 1 every time created
+                                                     //when add static, says this property is different than the ones that aren't marked status, there's only 1
+
         public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
@@ -17,10 +20,11 @@ namespace Movies
         public string Director { get; set; }
 
         public Movie() { } //default constructor, add now so don't get error later
-        public Movie(int id, string title, string genre, string rating,
-            int released, int runtime, string director)
+        public Movie(string title, string genre, string rating,
+            int released, int runtime, string director)  //new constructor with properties to initialize a movie with one line
+                                                         //id isn't in here because we don't want user generating/inputting
         {
-            this.Id = id;
+            this.Id = NextId++; //so now user can't set and will increment, other way around would be ++NextId = this.ID (increment and then assign)
             this.Title = title;
             this.Genre = genre;
             this.Rating = rating;
@@ -28,18 +32,40 @@ namespace Movies
             this.Runtime = runtime;
             this.Director = director; }
 
-            public int RuntimeMins(int hours, int mins) 
-        { 
-            return (hours * 60) + mins;
-        }
+        //example with only time this. *required*
+        /* public void SetRating(string Rating) <<confusing because parameter and property are the same name/spelling/capitalization
+         * {
+         * Rating = Rating; <<would come up with error
+         * change to: this.Rating = Rating; clear that left is property and right is parameter
+         * } */
+
+        //my code, makes more complicated bc tied to instance
+       /* public int RuntimeMins(int hours, int mins) //choice if you want to return something or void
+        {
+            return (hours * 60) + mins; //this.Runtime would store value into property, so would do public void
+        } */
+         public static int RuntimeMins(int hours, int mins)
+        {
+           return hours * 60 + mins;
+            }   //<<this works in program because you don't need var or instance, needs class name! 
+                    //if not static, will operate on one of the instance properties of the class so need var/instance to run
+                    //call using class name instead of instance name so can run before instance created
 
 
-            public void Print(int id, string title, string genre, string rating,
-            int released, int runtime, string director)
+
+            //my code, again much more complicated and makes it harder to work with data
+           /* public void Print(string title, string genre, string rating,
+            int released, int runtime, string director, int Id)
             {
                 Console.WriteLine($"Here's everything you need to know about {title}: Genre: {genre}, Rating: {rating}, " +
-                    $"Year Released: {released}, Runtime in Minutes: {runtime}, Director: {director}.");
-            }
+                    $"Year Released: {released}, Runtime in Minutes: {runtime}, Director: {director} ({Id}).");
+            } */
+
+        // how to print string
+         public string Print ()
+          {
+          return $"{Id} | {Title} | {Genre} | {Rating} | {Released} | {Runtime} | {Director}";
+         }
 
             //think about own life and build class for it and add class for it
         }
--- Movies/Program.cs
+++ Movies/Program.cs
@@ -7,55 +7,65 @@ namespace Movies
         static void Main(string[] args)
         {
             var LegallyBlonde = new Movie();
-            LegallyBlonde.Id = 1;
             LegallyBlonde.Title = "Legally Blonde";
             LegallyBlonde.Genre = "Comedy";
             LegallyBlonde.Rating = "PG-13";
             LegallyBlonde.Released = 2001;
-            LegallyBlonde.Runtime = LegallyBlonde.RuntimeMins(1, 36);
+            LegallyBlonde.Runtime = Movie.RuntimeMins(1, 36);
             LegallyBlonde.Director = "Robert Luketic";
-            LegallyBlonde.Print(LegallyBlonde.Id, LegallyBlonde.Title, LegallyBlonde.Genre, LegallyBlonde.Rating,
-            LegallyBlonde.Released, LegallyBlonde.Runtime, LegallyBlonde.Director);
+            
+            //the way to actually do this:
+                //var LegallyBlonde =
+                    //new Movie("Legally Blonde", "Comedy", "PG-13", 2001, LegallyBlonde.RuntimeMines(1, 36), "Luketic");
             var Clueless = new Movie();
-            Clueless.Id = 2;
             Clueless.Title = "Clueless";
             Clueless.Genre = "Comedy";
             Clueless.Rating = "PG-13";
             Clueless.Released = 1995;
-            Clueless.Runtime = Clueless.RuntimeMins(1, 37);
+            Clueless.Runtime = Movie.RuntimeMins(1, 37);
             Clueless.Director = "Amy Heckerling";
-            Clueless.Print(Clueless.Id, Clueless.Title, Clueless.Genre, Clueless.Rating, Clueless.Released, Clueless.Runtime, Clueless.Director);
             var CenterStage = new Movie();
-            CenterStage.Id = 3;
             CenterStage.Title = "Center Stage";
             CenterStage.Genre = "Drama";
             CenterStage.Rating = "PG-13";
             CenterStage.Released = 2000;
-            CenterStage.Runtime = CenterStage.RuntimeMins(1, 55);
+            CenterStage.Runtime = Movie.RuntimeMins(1, 55);
             CenterStage.Director = "Nicholas Hytner";
-            CenterStage.Print(CenterStage.Id, CenterStage.Title, CenterStage.Genre, CenterStage.Rating,
-                CenterStage.Released, CenterStage.Runtime, CenterStage.Director);
+            
             var TenThings = new Movie();
-            TenThings.Id = 4;
             TenThings.Title = "10 Things I Hate About You";
             TenThings.Genre = "Comedy";
             TenThings.Rating = "PG-13";
             TenThings.Released = 1999;
-            TenThings.Runtime = TenThings.RuntimeMins(1, 37);
+            TenThings.Runtime = Movie.RuntimeMins(1, 37);
             TenThings.Director = "Gil Junger";
-            TenThings.Print(TenThings.Id, TenThings.Title, TenThings.Genre, TenThings.Rating,
-                TenThings.Released, TenThings.Runtime, TenThings.Director);
+            
             var HowToLoseAGuy = new Movie();
-            HowToLoseAGuy.Id = 5;
             HowToLoseAGuy.Title = "How To Los a Guy in 10 Days";
             HowToLoseAGuy.Genre = "Romantic Comedy";
             HowToLoseAGuy.Rating = "PG-13";
             HowToLoseAGuy.Released = 2003;
-            HowToLoseAGuy.Runtime = HowToLoseAGuy.RuntimeMins(1, 56);
+            HowToLoseAGuy.Runtime = Movie.RuntimeMins(1, 56);
             HowToLoseAGuy.Director = "Donald Petrie";
-            HowToLoseAGuy.Print(HowToLoseAGuy.Id, HowToLoseAGuy.Title, HowToLoseAGuy.Genre, HowToLoseAGuy.Rating,
-                HowToLoseAGuy.Released, HowToLoseAGuy.Runtime, HowToLoseAGuy.Director);
 
+
+            Console.WriteLine(LegallyBlonde.Print());
+            Console.WriteLine(Clueless.Print());
+            Console.WriteLine(CenterStage.Print());
+            Console.WriteLine(TenThings.Print());
+            Console.WriteLine(HowToLoseAGuy.Print());
+
+            //what I did...wrong (even tho it worked)
+            /*
+            LegallyBlonde.Print(LegallyBlonde.Title, LegallyBlonde.Genre, LegallyBlonde.Rating,
+            LegallyBlonde.Released, LegallyBlonde.Runtime, LegallyBlonde.Director, LegallyBlonde.Id);
+            Clueless.Print(Clueless.Title, Clueless.Genre, Clueless.Rating, Clueless.Released, Clueless.Runtime, Clueless.Director, Clueless.Id);
+            CenterStage.Print(CenterStage.Title, CenterStage.Genre, CenterStage.Rating,
+                CenterStage.Released, CenterStage.Runtime, CenterStage.Director, CenterStage.Id);
+            TenThings.Print(TenThings.Title, TenThings.Genre, TenThings.Rating,
+                TenThings.Released, TenThings.Runtime, TenThings.Director, TenThings.Id);
+            HowToLoseAGuy.Print(HowToLoseAGuy.Title, HowToLoseAGuy.Genre, HowToLoseAGuy.Rating,
+                HowToLoseAGuy.Released, HowToLoseAGuy.Runtime, HowToLoseAGuy.Director, HowToLoseAGuy.Id); */
 
 
 
